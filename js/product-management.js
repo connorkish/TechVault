f0e0bf21e0974a90061a0e834bf11c2a0d3c7627
connorkish <connorkish@outlook.com>
2022-03-19 22:23:46 -0400

    Reformatted HTML and Css to reflect Product Management Page design discussed in class, added javascript to enable/disable buttons and forms fields, and hide/show buttons depending on what the action the user chooses to perform

--- js/product-management.js
+++ js/product-management.js
@@ -6,6 +6,8 @@
 // Last Updated By: Connor Kish
 
 var productID,
+	manufacturer,
+	productname,
 	description,
     category,
     quantity,
@@ -15,6 +17,8 @@ var productID,
 // Variable to represent a JSON object to store account details
 var productDetails = {
 	"productID":"",
+	"manufacturer":"",
+	"productname":"",
 	"description": "",
 	"category": "",
 	"quantity": "",
@@ -22,11 +26,93 @@ var productDetails = {
 	"weight": ""
 };
 
-function editProductData() {
+const newProduct = document.getElementById("new-product");
+const editProduct = document.getElementById("edit-product");
+const deleteProduct = document.getElementById("delete-product");
+const formactions = document.getElementById("form-actions");
+
+function disableActionButtons() {
+	const newProduct = document.getElementById("new-product").disabled = true;
+	const editProduct = document.getElementById("edit-product").disabled = true;
+	const deleteProduct = document.getElementById("delete-product").disabled = true;
+}
+
+function enableActionButtons() {
+	const newProduct = document.getElementById("new-product").disabled = false;
+	const editProduct = document.getElementById("edit-product").disabled = false;
+	const deleteProduct = document.getElementById("delete-product").disabled = false;
+}
+
+function disableForm() {
+	document.getElementById("ProductID").disabled = true;
+	document.getElementById("Manufacturer").disabled = true;
+	document.getElementById("ProductName").disabled = true;
+	document.getElementById("ProductDescription").disabled = true;
+	document.getElementById("Category").disabled = true;
+	document.getElementById("Quantity").disabled = true;
+	document.getElementById("Price").disabled = true;
+	document.getElementById("Weight").disabled = true;
+}
+
+function enableForm() {
+	document.getElementById("ProductID").disabled = false;
+	document.getElementById("Manufacturer").disabled = false;
+	document.getElementById("ProductName").disabled = false;
+	document.getElementById("ProductDescription").disabled = false;
+	document.getElementById("Category").disabled = false;
+	document.getElementById("Quantity").disabled = false;
+	document.getElementById("Price").disabled = false;
+	document.getElementById("Weight").disabled = false;
+}
+
+function newProductAction() {
+	disableActionButtons();
+	enableForm();
+	formactions.innerHTML = "";
+	formactions.innerHTML = '<button type="submit" class="btn btn-primary" id="save" onClick="confirmSave()">Save</button>' +
+							'<button type="submit" class="btn btn-primary" id="cancel" onClick="cancelAction()">Cancel</button>';
 	
 }
 
-function disableSearch() {
-	alert("search disabled");
-	document.getElementById("product-search").disabled = true;
+function updateProductAction() {
+	disableActionButtons();
+	enableForm();
+	formactions.innerHTML = "";
+	formactions.innerHTML = '<button type="submit" class="btn btn-primary" id="update" onClick="confirmUpdate()">Update</button>' +
+							'<button type="submit" class="btn btn-primary" id="cancel" onClick="cancelAction()">Cancel</button>';
+	
+}
+
+function deleteProductAction() {
+	disableActionButtons();
+	enableForm();
+	formactions.innerHTML = "";
+	formactions.innerHTML = '<button type="submit" class="btn btn-primary" id="delete" onClick="confirmDelete()">Delete</button>' +
+							'<button type="submit" class="btn btn-primary" id="cancel" onClick="cancelAction()">Cancel</button>';
+	
+}
+
+function confirmSave() {
+	formactions.reset();
+	enableActionButtons();
+	formactions.innerHTML = "";
+}
+
+function confirmUpdate() {
+	formactions.reset();
+	enableActionButtons();
+	formactions.innerHTML = "";
+}
+
+function confirmDelete() {
+	formactions.reset();
+	enableActionButtons();
+	formactions.innerHTML = "";
+}
+
+function cancelAction() {
+	formactions.reset();
+	enableActionButtons();
+	formactions.innerHTML = "";
+	
 }
